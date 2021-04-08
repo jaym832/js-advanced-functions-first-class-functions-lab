1913faae1f005e09165cb3d5e5bbde00a2f4d99c
Javier Marin <jjmarin777@gmail.com>
2021-04-08 14:03:12 -0500

    commit tree b09b69124fa2affbe882ee41ca20db4082b29b81

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers= function (arr){
+    let newArr=arr.slice(0,2);
+    return newArr;
+    
+}
+
+const returnLastTwoDrivers=function(arr){
+    let newArr= arr.slice(2,4);
+    return newArr;
+}
+
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplyBy){
+    let multiplier= function(fare){
+        return multiplyBy*fare;
+    }
+    return multiplier;
+}
+
+const fareDoubler= createFareMultiplier(2);
+
+const fareTripler= createFareMultiplier(3);
+
+function selectDifferentDrivers(arr,aFunction){
+    return aFunction(arr);
+
+}
